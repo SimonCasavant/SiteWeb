88869f95528991d8d1e37630db1ce84a08864d57
SimonCasavant <112723946+SimonCasavant@users.noreply.github.com>
2023-01-31 13:02:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,28 @@
 function myFunction() {
     document.getElementById("pop-up").style.display = "none";
   }
+
+function progressBarScroll() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+      height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+      scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+window.onscroll = function () {
+  progressBarScroll();
+};
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+const hiddenElements =  document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
